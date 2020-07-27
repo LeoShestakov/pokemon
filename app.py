8c0093ed0b433c75267e396eb500abf359debec9
NerdyMonarch <npandey@etech-nyc.org>
2020-07-27 12:00:55 -0400

    Database Init

--- app.py
+++ app.py
@@ -4,17 +4,34 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import session
+from flask import url_for
+from flask import redirect
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
+import datetime
 
 import team
 import api
 
 import requests #To access our API
 
+
+load_dotenv()
 # -- Initialization section --
 app = Flask(__name__)
 
 ## secret key for session (In production, you would set this key via an environment variable)
 app.secret_key = b'HO\xf8\xff+\n\x1e\\~/;}'
+
+MONGO_DBNAME = os.getenv("MONGO_DBNAME")
+MONGO_DB_USERNAME = os.getenv("MONGO_DB_USERNAME")
+MONGO_DB_PASSWORD = os.getenv("MONGO_DB_PASSWORD")
+
+app.config['MONGO_DBNAME'] = MONGO_DBNAME
+app.config['MONGO_URI'] = f"mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@cluster0.b4bsb.mongodb.net/{MONGO_DBNAME}?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 # -- Routes section --
 @app.route('/')
@@ -26,4 +43,9 @@ def index():
         form = list(request.form)
         my_team = team.Team()
         my_team.add_pokemon(form)
+        db_team = {
+            "team": my_team.get_names()
+        }
+        mongo.db.teams.insert(db_team)
         return render_template('results.html', data=my_team)
+
