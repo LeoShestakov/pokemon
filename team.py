4b9f0f99cf98dc9d17815be581927725a411bbaf
LeoShestakov <leo.shestakov@hotmail.com>
2020-07-27 11:40:09 -0400

    finished type chart

--- team.py
+++ team.py
@@ -1,5 +1,37 @@
 import api
+import numpy as np
 
+### A Pokemon Types Table Plot In Python
+### Seaborn Approach:
+# References:
+# https://stackoverflow.com/questions/33158075/custom-annotation-seaborn-heatmap
+# https://stackoverflow.com/questions/40734343/artificial-tick-labels-for-seaborn-heatmaps
+# Pokemon Type Table Reference: https://img.pokemondb.net/images/typechart.png
+
+pokemon_types = {"Normal" : 0, "Fire" : 1, "Water" : 2, "Electric" : 3, "Grass" : 4, "Ice" : 5,
+                 "Fighting" : 6, "Poison" : 7, "Ground" : 8, "Flying" : 9, "Psychic" : 10,
+                 "Bug" : 11, "Rock" : 12, "Ghost" : 13, "Dragon" : 14, "Dark" : 15, "Steel" : 16, "Fairy" : 17}
+
+# A 2 Dimenstional Numpy Array Of Damage Multipliers For Attacking Pokemon:
+
+damage_array = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1/2, 0, 1, 1, 1/2, 1],
+                    [1, 1/2, 1/2, 1, 2, 2, 1, 1, 1, 1, 1, 2, 1/2, 1, 1/2, 1, 2, 1],
+                    [1, 2, 1/2, 1, 1/2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1/2, 1, 1, 1],
+                    [1, 1, 2, 1/2, 1/2, 1, 1, 1, 0, 2, 1, 1, 1, 1, 1/2, 1, 1, 1],
+                    [1, 1/2, 2, 1, 1/2, 1, 1, 1/2, 2, 1/2, 1, 1/2, 2, 1, 1/2, 1, 1/2, 1],
+                    [1, 1/2, 1/2, 1, 2, 1/2, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 1/2, 1],
+                    [2, 1, 1, 1, 1, 2, 1, 1/2, 1, 1/2, 1/2, 1/2, 2, 0, 1, 2, 2, 1/2],
+                    [1, 1, 1, 1, 2, 1, 1, 1/2, 1/2, 1, 1, 1, 1/2, 1/2, 1, 1, 0, 2],
+                    [1, 2, 1, 2, 1/2, 1, 1, 2, 1, 0, 1, 1/2, 2, 1, 1, 1, 2, 1],
+                    [1, 1, 1, 1/2, 2, 1, 2, 1, 1, 1, 1, 2, 1/2, 1, 1, 1, 1/2, 1],
+                    [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1/2, 1, 1, 1, 1, 0, 1/2, 1],
+                    [1, 1/2, 1, 1, 2, 1, 1/2, 1/2, 1, 1/2, 2, 1, 1, 1/2, 1, 2, 1/2, 1/2],
+                    [1, 2, 1, 1, 1, 2, 1/2, 1, 1/2, 2, 1, 2, 1, 1, 1, 1, 1/2, 1],
+                    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1/2, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1/2, 0],
+                    [1, 1, 1, 1, 1, 1, 1/2, 1, 1, 1, 2, 1, 1, 2, 1, 1/2, 1, 1/2],
+                    [1, 1/2, 1/2, 1/2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1/2, 2],
+                    [1, 1/2, 1, 1, 1, 1, 2, 1/2, 1, 1, 1, 1, 1, 1, 2, 2, 1/2, 1]])
 class Team:
     def __init__(self):
         self.pokemon = {}
@@ -18,6 +50,45 @@ class Team:
 
     def get_pokemon(self):
         return self.pokemon
+
+    def get_type(self, name):
+        data = self.pokemon[name]['types']
+        ans = []
+        for poke_type in data:
+            ans.append(poke_type['type']['name'].capitalize())
+        return ans
+
+    def get_types(self):
+        types = []
+        for mon in self.pokemon:
+            types.extend(self.get_type(mon))
+        return list(set(types))
+
+    def get_weaknesses(self):
+        my_types = self.get_types()
+        weaknesses = []
+        keys = list(pokemon_types.keys())
+        for poke_type in my_types:
+            def_index = pokemon_types[poke_type]
+            for i in range(len(damage_array)):
+                if damage_array[i][def_index] == 2:
+                    weaknesses.append(keys[i])
+        weaknesses = list(set(weaknesses))
+        return weaknesses
+
+    def get_unchecked(self):
+        weaknesses = self.get_weaknesses()
+        countered = []
+        for weakness in weaknesses:
+            for type in self.get_types():
+                atk_index = pokemon_types[type]
+                def_index = pokemon_types[weakness]
+                if damage_array[atk_index][def_index] == 2:
+                    countered.append(weakness)
+                    break
+        for not_weak in countered:
+            weaknesses.remove(not_weak)
+        return weaknesses
 
     def get_picture(self, name):
         image = self.pokemon[name]['sprites']['front_default']
---
+++ test.py
@@ -0,0 +1,7 @@
+import team
+
+my_team = team.Team()
+my_team.add_pokemon(['bulbasaur', 'charizard'])
+print(my_team.get_types())
+print(my_team.get_weaknesses())
+print(my_team.get_unchecked())
