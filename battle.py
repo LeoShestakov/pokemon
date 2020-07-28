6ef45a0d8257cdc2b3feebfee40310a8cd87f464
LeoShestakov <leo.shestakov@hotmail.com>
2020-07-28 12:30:09 -0400

    small change

--- battle.py
+++ battle.py
@@ -4,15 +4,13 @@ class Battle:
     def __init__(self, player1, player2)
     self.team1 = player1 # team objects
     self.team2 = player2
-    self.life1 = [1, 1, 1, 1, 1, 1]
-    self.life2 = [1, 1, 1, 1, 1, 1]
 
     def start(self):
         var = True
         while var:
             self.get_input(self.player1)
             # player1 makes move
-            if self.life2 != [0, 0, 0, 0, 0, 0]:
+            # if player2 is not dead
                 self.get_input(self.player2)
 
     def get_input(self, player):
